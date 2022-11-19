b00a53b0e48389bddf0742e56b5e7a3d608f4c37
Wofa8 <101602442+Wofa8@users.noreply.github.com>
2022-11-19 22:52:53 +0000

    Add files via upload

---
+++ forge_test_py.py
@@ -0,0 +1,7 @@
+import matplotlib as mplt
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+
+img = mpimg.imread("C:\\Users\\yamoa\\OneDrive - Ashesi University\\Ashesi\\Year 2\\Semester 1 Course Material\\Discrete Structures and Theory\\Discrete Final Project\\rabbit_1.png")
+print(img)
